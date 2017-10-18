6fa34884535a37664b1c53ca0d019ca96c4a8b09
Nikolay Astakhov <astakhov.na@gmail.com>
2017-10-18 23:44:02 +0300

    Sameness

--- src/App/src/Action/HomePageFactory.php
+++ src/App/src/Action/HomePageFactory.php
@@ -4,13 +4,14 @@ namespace App\Action;
 
 use Interop\Container\ContainerInterface;
 use Zend\Expressive\Template\TemplateRendererInterface;
+use App\Model\Post;
 
 class HomePageFactory
 {
     public function __invoke(ContainerInterface $container)
     {
         $template = $container->get(TemplateRendererInterface::class);
-        $posts = $container->get('App\Model\Post')->fetchLast();
+        $posts = $container->get(Post::class)->fetchLast();
 
         return new HomePageAction($template, $posts);
     }
--- src/App/src/Model/ModelFactory.php
+++ src/App/src/Model/ModelFactory.php
@@ -3,11 +3,12 @@
 namespace App\Model;
 
 use Interop\Container\ContainerInterface;
+use Zend\Db\Adapter\Adapter as DbAdapter;
 
 class ModelFactory
 {
     public function __invoke(ContainerInterface $container, $requestedName)
     {
-        return new $requestedName($container->get('Zend\Db\Adapter\Adapter'));
+        return new $requestedName($container->get(DbAdapter::class));
     }
 }
