d58774503b52b063016c300508d275497106f196
Nikolay Astakhov <astakhov.na@gmail.com>
2017-10-25 23:10:25 +0300

    Using sql abstraction

--- src/App/src/Model/Post.php
+++ src/App/src/Model/Post.php
@@ -2,12 +2,22 @@
 
 namespace App\Model;
 
+use Zend\Db\Sql\Sql;
+
 class Post extends Model
 {
 
   public function fetchLast()
   {
-    $statement = $this->db->query('select id, title, short_text from posts order by id limit 3;');
+    $sql = new Sql($this->db);
+    $select = $sql->select();
+    $select
+        ->from('posts')
+        ->columns(['id', 'title', 'short_text'])
+        ->order('id')
+        ->limit(3);
+
+    $statement = $sql->prepareStatementForSqlObject($select);
     return $statement->execute();
   }
 
