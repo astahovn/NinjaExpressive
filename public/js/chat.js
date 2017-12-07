9756d1406c3dbea63760ef957206984c847f2097
Nikolay Astakhov <astakhov.na@gmail.com>
2017-12-07 22:52:49 +0300

    Encrypt chat key with rsa open key

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,14 +4,26 @@ $.when( $.ready ).then(function() {
     });
 
     $('#chat_create_form').submit(function(event) {
+        event.preventDefault();
+        var
+            $themeError = $('#theme-error'),
+            $theme = $('#theme'),
+            $openKey = $('#active-user-open-key');
+
+        $themeError.addClass('hidden').html('');
+
         // Get chat theme
-        var theme = $('#theme').val();
+        var theme = $theme.val();
+        if (!theme.trim()) {
+            $themeError.html('The theme is empty').removeClass('hidden');
+            return;
+        }
         // Generate chat key
         var key = ninjaCrypto.createKey();
         // Encrypt chat theme with the key
-        var encTheme = ninjaCrypto.encryptText(theme, key);
+        var encTheme = ninjaCrypto.encryptTripleDES(theme, key);
         // Encrypt chat key with open key of current user
-        // ...
-        event.preventDefault();
+        var openKey = $openKey.html();
+        var encKey = ninjaCrypto.encryptRsa(key, openKey);
     });
 });
--- public/js/crypto.js
+++ public/js/crypto.js
@@ -8,14 +8,26 @@ var ninjaCrypto = ninjaCrypto || (function() {
         return hash.toString();
     };
 
-    c.encryptText = function(message, key) {
+    c.encryptTripleDES = function(message, key) {
         var encrypted = CryptoJS.TripleDES.encrypt(message, key);
         return encrypted.toString();
     };
 
-    c.decryptText = function(encrypted, key) {
+    c.decryptTripleDES = function(encrypted, key) {
         var decrypted = CryptoJS.TripleDES.decrypt(encrypted, key);
         return decrypted.toString(CryptoJS.enc.Utf8);
+    };
+
+    c.encryptRsa = function(message, openKey) {
+        var encrypt = new JSEncrypt();
+        encrypt.setPublicKey(openKey);
+        return encrypt.encrypt(message);
+    };
+
+    c.decryptRsa = function(encrypted, privateKey) {
+        var decrypt = new JSEncrypt();
+        decrypt.setPrivateKey(privateKey);
+        return decrypt.decrypt(encrypted);
     };
 
     return c;
