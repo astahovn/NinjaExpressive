12ae51d94b838783c166f84cd8374cad81003ce1
Nikolay Astakhov <astakhov.na@gmail.com>
2017-12-12 23:01:17 +0300

    Profile common object

--- public/js/profile/common.js
+++ public/js/profile/common.js
@@ -1,34 +1,49 @@
-$.when( $.ready ).then(function() {
-    if (!sessionStorage.getItem('private_key')) {
-        var fileInput = document.getElementById('private_selector_file_input');
-        var fileDisplayArea = document.getElementById('private_selector_error');
+Application.modules.profileCommon = (function(self) {
+    var
+        _addEvents = function() {
+            $(document)
+                .on('click', '#button_logout', function(event) {
+                    sessionStorage.clear();
+                    window.location.href = '/logout';
+                    event.preventDefault();
+                });
+        },
 
-        fileInput.addEventListener('change', function(e) {
-            var file = fileInput.files[0];
-            var textType = /.*-x509-ca-cert/;
-
-            if (file.type.match(textType)) {
-                var reader = new FileReader();
-
-                reader.onload = function(e) {
-                    sessionStorage.setItem('private_key', reader.result);
-                    window.location.reload(false);
-                };
-
-                reader.readAsText(file);
-            } else {
-                fileDisplayArea.innerText = "File not supported!"
+        _autoCheckPrivateKey = function() {
+            if (sessionStorage.getItem('private_key')) {
+                return;
             }
-        });
+            var fileInput = document.getElementById('private_selector_file_input');
+            var fileDisplayArea = document.getElementById('private_selector_error');
 
-        $('#private_selector').modal({
-            backdrop: false
-        });
-    }
+            fileInput.addEventListener('change', function(e) {
+                var file = fileInput.files[0];
+                var textType = /.*-x509-ca-cert/;
 
-    $('#button_logout').click(function(event) {
-        sessionStorage.clear();
-        window.location.href = '/logout';
-        event.preventDefault();
-    });
-});
+                if (file.type.match(textType)) {
+                    var reader = new FileReader();
+
+                    reader.onload = function(e) {
+                        sessionStorage.setItem('private_key', reader.result);
+                        window.location.reload(false);
+                    };
+
+                    reader.readAsText(file);
+                } else {
+                    fileDisplayArea.innerText = "File not supported!"
+                }
+            });
+
+            $('#private_selector').modal({
+                backdrop: false
+            });
+        }
+    ;
+
+    self.load = function() {
+        _addEvents();
+        _autoCheckPrivateKey();
+    };
+
+    return self;
+}({}));
