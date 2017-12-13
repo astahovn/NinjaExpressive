0d7588ab5e6e480f14240d8141fba4b4916e40a2
Nikolay Astakhov <astakhov.na@gmail.com>
2017-12-13 23:27:31 +0300

    Hello world with reactjs

---
+++ public/js/profile/test.js
@@ -0,0 +1,34 @@
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <strong>{this.state.date.toLocaleTimeString()}</strong>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('time')
+);
