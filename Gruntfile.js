cdecfcb414114924da26ab61d9ec820219389a05
Nikolay Astakhov <astakhov.na@gmail.com>
2018-02-27 22:45:51 +0300

    Building js module files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,33 @@ module.exports = function(grunt) {
     require("load-grunt-tasks")(grunt); // npm install --save-dev load-grunt-tasks
 
     grunt.initConfig({
-        "babel": {
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        'public/js/mod_profile.js'
+                    ]
+                }]
+            },
+            server: '.tmp'
+        },
+
+        concat: {
+            options: {
+                sourceMap: true,
+                sourceMapStyle: 'link',
+                separator: ';',
+            },
+            dist: {
+                files: {
+                    '.tmp/mod_profile.js': ['assets/js/profile/*.js'],
+                }
+            },
+        },
+
+        babel: {
             options: {
                 sourceMap: true,
                 presets: ['env'],
@@ -11,11 +37,15 @@ module.exports = function(grunt) {
             },
             dist: {
                 files: {
-                    "public/js/profile/test.min.js": "public/js/profile/test.js"
+                    "public/js/mod_profile.js": ".tmp/mod_profile.js",
                 }
             }
         }
     });
 
-    grunt.registerTask("default", ["babel"]);
+    grunt.registerTask('default', [
+        'clean',
+        'concat',
+        'babel'
+    ]);
 };
