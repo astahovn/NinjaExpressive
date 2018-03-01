9fc99c275370dafaba86dbd9cadb1440fe21d017
Nikolay Astakhov <astakhov.na@gmail.com>
2018-03-01 23:07:13 +0300

    Conversations profile widget

---
+++ assets/js/profile/ConversationsWidget.js
@@ -0,0 +1,29 @@
+class ConversationsWidget extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let privateKey = sessionStorage.getItem('private_key');
+        if (!privateKey) {
+            return <div>You should load a private key</div>;
+        }
+        let conversations = JSON.parse(this.props.conversations);
+        let resultConversations = [];
+        for (let i = 0; i < conversations.length; i++) {
+            let conversation = conversations[i];
+            let key = ninjaCrypto.decryptRsa(conversation.key, privateKey);
+            let theme = ninjaCrypto.decryptTripleDES(conversation.theme, key);
+            resultConversations.push({
+                'url': '/conversation/' + conversation.id,
+                'theme': theme
+            });
+        }
+
+        return (
+            <div>
+                {resultConversations.map((item) => [<a href={item.url}>{item.theme}</a>, <br />])}
+            </div>
+        );
+    }
+}
